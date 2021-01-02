e480d8b07eb62e6c8f224a71c0950759aee7f746
agussuparman <agus.soeparmans@gmail.com>
2021-01-02 12:55:11 +0700

    attr

---
+++ 4-attr.rb
@@ -0,0 +1,30 @@
+class Printer
+	attr_accessor :text
+	# attr_writer :text
+	# attr_reader :text
+
+	def initialize(text)
+		@text = text
+	end
+
+	# # Setter
+	# def text=(text)
+	# 	@text = text
+	# end
+
+	# # Getter
+	# def text
+	# 	"Menggunakan getter: #{@text}"
+	# end
+
+	def print
+		puts @text
+	end
+end
+
+printer = Printer.new("Saya sedang belajar Ruby")
+printer.print
+printer.text=("Ruby adalah bahasa yang menyenangkan") # Menggunakan setter untuk mengeset nilai
+printer.print
+printer.text = "ini adalah seetter dan getter" # Cara kedua untuk menggunakan setter untuk mengeset nilai
+puts printer.text # Menggunakan getter
